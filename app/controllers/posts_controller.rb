b1cf8672de1c00fab9b30f45422f6878518357be
raziel0110 <albangabriel42@gmail.com>
2021-11-08 19:01:21 +0200

    post feature in progress. index done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = 'Post successfully saved.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update
+      flash[:notice] = 'Post successfully updated.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Something went wrong.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
